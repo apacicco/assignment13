b77c3d0564be74de92c8e761d6dc153015fa2c4a
apacicco <apacicco@email.sc.edu>
2023-11-05 23:18:49 -0500

    what

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 3001;
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("https://hw13.onrender.com ");//  http://localhost:3000
+    res.end("http://localhost:3000");//  http://localhost:3000
   })
   .listen(port);
